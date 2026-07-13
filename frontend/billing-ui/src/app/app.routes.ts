cd77131c3bf3dd531fd1d8eb3d4a78a4e1c972f7
srijak1000 <your-personal-email@example.com>
2026-07-13 22:37:01 +0530

    Routes added

--- frontend/billing-ui/src/app/app.routes.ts
+++ frontend/billing-ui/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { InvoicePageComponent } from './features/invoice/pages/invoice-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: InvoicePageComponent },
+  { path: '**', redirectTo: '' }
+];
