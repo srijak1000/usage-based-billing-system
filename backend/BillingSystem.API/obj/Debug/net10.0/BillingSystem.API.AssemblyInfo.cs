f0b23e360f05feca7c8bb1333b480f52a66a7651
srijak1000 <your-personal-email@example.com>
2026-07-13 22:59:27 +0530

    Readme updated

--- backend/BillingSystem.API/obj/Debug/net10.0/BillingSystem.API.AssemblyInfo.cs
+++ backend/BillingSystem.API/obj/Debug/net10.0/BillingSystem.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BillingSystem.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f191924135e95f9d9185a4ba0d10be7d98126aee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66f8447adb00957790fcf25b112ae33a2b4c9cde")]
 [assembly: System.Reflection.AssemblyProductAttribute("BillingSystem.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BillingSystem.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
