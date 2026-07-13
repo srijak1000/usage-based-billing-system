3a88b33f6e5b3a572586e15375fd73660a2b9b1c
srijak1000 <your-personal-email@example.com>
2026-07-13 22:03:33 +0530

    Template components generated

---
+++ frontend/billing-ui/src/app/features/invoice/components/invoice-form/invoice-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-invoice-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './invoice-form.component.html',
+  styleUrl: './invoice-form.component.scss'
+})
+export class InvoiceFormComponent {
+
+}
---
+++ frontend/billing-ui/src/app/features/invoice/pages/invoice-page/invoice-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-invoice-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './invoice-page.component.html',
+  styleUrl: './invoice-page.component.scss'
+})
+export class InvoicePageComponent {
+
+}
