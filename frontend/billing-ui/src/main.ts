e15c689aebabb4dd6dc91d9a2478eba9122ca6ec
srijak1000 <your-personal-email@example.com>
2026-07-13 23:16:22 +0530

    main.ts updated

--- frontend/billing-ui/src/main.ts
+++ frontend/billing-ui/src/main.ts
@@ -1,10 +1,5 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
+import { appConfig } from './app/app.config';
 
-import { provideHttpClient } from '@angular/common/http';
-
-bootstrapApplication(AppComponent, {
-  providers: [
-    provideHttpClient()
-  ]
-});
+bootstrapApplication(AppComponent, appConfig);
